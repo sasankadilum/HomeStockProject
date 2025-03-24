49c7e00373e216ca578ae2f6ef75643cacaf826e
sasanka dilum chamuditha <sasankadilum372@gmail.com>
2025-03-25 01:28:35 +0530

    Update UserController.js

--- BACKEND/Controller/UserController.js
+++ BACKEND/Controller/UserController.js
@@ -3,35 +3,7 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 // Signup a new user
-// export const signup = async (req, res) => {
-//   const { name, email, password, role } = req.body;
 
-//   try {
-//     // Check if user already exists
-//     const userExists = await User.findOne({ email });
-//     if (userExists) {
-//       return res.status(400).json({ message: "User already exists" });
-//     }
-
-//     // Create user (password will be hashed by the pre-save hook)
-//     const user = await User.create({
-//       name,
-//       email,
-//       password, // Pass the plain password
-//       role: role || "user", // Default role is "user"
-//     });
-
-//     // Generate JWT
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//       expiresIn: "30d",
-//     });
-
-//     res.status(201).json({ token, user });
-//   } catch (error) {
-//     console.error("Signup Error:", error); // Debugging
-//     res.status(500).json({ message: "Server error", error: error.message });
-//   }
-// };
 export const signup = async (req, res) => {
   const { name, email, password, role } = req.body;
 
